34919252691dedbe0504ce56c8f00f9700193443
TaeChan <xocksdlekd@gmail.com>
2019-12-30 17:50:50 +0900

    jpa 실습 Member객체 컬럼추가 DDL 자동생성 기능 추가

--- src/main/java/com/myapp/jpa/Member.java
+++ src/main/java/com/myapp/jpa/Member.java
@@ -1,7 +1,21 @@
 package com.myapp.jpa;
 
-import javax.persistence.*;  //**
-import lombok.*;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.myapp.common.RoleType;
+
+import lombok.Getter;
+import lombok.Setter;
 /**
  * User: HolyEyE
  * Date: 13. 5. 24. Time: 오후 7:43
@@ -15,8 +29,21 @@ public class Member {
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10)
     private String username;
 
     private Integer age;
+    
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    
+    @Lob
+    private String description;
 }
+
