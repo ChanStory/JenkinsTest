8caeec05c9a6c3b7bfb7d12e605cf10b55646814
TaeChan <xocksdlekd@gmail.com>
2019-12-30 17:06:43 +0900

    롬복 라이브러리 적용

--- src/main/java/com/myapp/jpa/Member.java
+++ src/main/java/com/myapp/jpa/Member.java
@@ -1,13 +1,14 @@
 package com.myapp.jpa;
 
 import javax.persistence.*;  //**
-
+import lombok.*;
 /**
  * User: HolyEyE
  * Date: 13. 5. 24. Time: 오후 7:43
  */
 @Entity
 @Table(name="MEMBER")
+@Getter @Setter
 public class Member {
 
     @Id
@@ -18,28 +19,4 @@ public class Member {
     private String username;
 
     private Integer age;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
