b574631ee7dcfa932aab0da98beed37039832f0e
TaeChan <xocksdlekd@gmail.com>
2020-02-12 23:45:16 +0900

    api restful하게 수정

--- src/main/java/com/myapp/controller/LoginController.java
+++ src/main/java/com/myapp/controller/LoginController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,25 +21,26 @@ import com.myapp.service.LoginService;
  */
 @CrossOrigin(origins = "*", allowedHeaders = "*")//크로스 도메인 해결을 위한 어노테이션
 @RestController
+@RequestMapping("/")
 public class LoginController {
 
 	@Autowired
 	LoginService loginService;
 	
 	//id 중복체크
-	@RequestMapping("/id-duplicate")
+	@GetMapping("/id-duplicate")
 	public JSONObject idDuplicateCheck(@RequestParam Map params) {
 		return loginService.idDuplicateCheck((String) params.get("joinId"));
 	}
 	
 	//로그인
-	@RequestMapping("/login")
-	public JSONObject login(@RequestBody Map body) {
-		return loginService.login(body);
+	@GetMapping("/login")
+	public JSONObject login(@RequestParam Map params) {
+		return loginService.login(params);
 	}
 	
 	//회원가입
-	@RequestMapping("/join")
+	@PostMapping("/user")
 	public JSONObject join(@RequestBody Map body) {
 		return loginService.join(body);
 	}
